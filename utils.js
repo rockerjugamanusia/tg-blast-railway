795b514f108cba7485ed728ab4b199edc1e8e98e
rockerjugamanusia <mktgsp123@gmail.com>
2026-02-08 15:28:20 +0700

    utils.js

--- utils.js
+++ utils.js
@@ -1,23 +1,13 @@
-export function mustEnv(name) {
-  const v = process.env[name];
-  if (!v) throw new Error(`${name} belum diset`);
-  return v;
+export function isAdminTelegram(ctx) {
+  const adminId = Number(process.env.ADMIN_ID || 0);
+  if (!adminId) return true; // kalau tidak diset, semua boleh
+  return Number(ctx?.from?.id) === adminId;
+}
+
+export function errText(e) {
+  return (e?.response?.description || e?.description || e?.message || String(e));
 }
 
 export function delay(ms) {
   return new Promise((r) => setTimeout(r, ms));
-}
-
-export function errText(e) {
-  const msg = e?.message || String(e);
-  const desc = e?.response?.description || e?.description;
-  return desc ? `${msg} | ${desc}` : msg;
-}
-
-export function requireAdminKey(req, res, next) {
-  const key = req.headers["x-admin-key"] || req.query.key || "";
-  const ADMIN_KEY = process.env.ADMIN_KEY || "";
-  if (!ADMIN_KEY) return res.status(500).send("ADMIN_KEY belum diset");
-  if (key !== ADMIN_KEY) return res.status(401).send("Unauthorized");
-  next();
 }
